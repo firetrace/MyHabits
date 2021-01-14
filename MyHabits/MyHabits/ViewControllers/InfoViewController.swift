efb71b6c9dea32dda891fdf1bf7cbdba08600908
Aleksandr Glazkov <dev@alexglazkov.ru>
2021-01-14 15:31:59 +0300

    add Info

--- MyHabits/MyHabits/ViewControllers/InfoViewController.swift
+++ MyHabits/MyHabits/ViewControllers/InfoViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
+    private lazy var infoView: InfoView =  {
+        var view = InfoView(frame: .zero)
+        view.toAutoLayout()
+        
+        return view;
+    }();
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        setupLayout()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,5 +30,14 @@ class InfoViewController: UIViewController {
         
         tabBarController?.navigationItem.title = "Информация"
         tabBarController?.navigationItem.rightBarButtonItem = nil
+    }
+    
+    private func setupLayout() {
+        view.addSubview(infoView)
+        
+        NSLayoutConstraint.activate([infoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                                     infoView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                                     infoView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                                     infoView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
     }
 }
